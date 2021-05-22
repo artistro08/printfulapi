372d9c2a1f728dd6e849740ee2467e5d4c875b34
Devin Green <design@devingreen.me>
2021-05-22 10:35:41 -0500

    added more qualified validation to rememve fields based on selection. Added more comments to the field.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,25 +2,47 @@
 $(document).ready(function () {
     $(document).on('shown.oc.popup', function() {
         // Get form and check if fields are selected, and disable if they are
-        $('.variantPlacementOptions select').on('change', function() {
+        $('.variantPlacementOptions').on('change', 'select', function() {
+            console.log('did it work?')
             $('.variantPlacementOptions select option').prop('disabled', false);
             $('.variantPlacementOptions select').each(function() {
-                var val = this.value;
+                let val = this.value;
                 $('.variantPlacementOptions select').not(this).find('option').filter(function() {
                     return this.value === val;
-                }).prop('disabled', true);
+                }, this).prop('disabled', true);
+            });
+        }).change();
+
+        $('.variantPlacementOptions select').on('change', function() {
+            // console.log('did it work?')
+            $('.variantPlacementOptions select option').prop('disabled', false);
+            $('.variantPlacementOptions select').each(function() {
+                let val = this.value;
+                $('.variantPlacementOptions select').not(this).find('option').filter(function() {
+                    return this.value === val;
+                }, this).prop('disabled', true);
             });
         }).change();
 
         // Get form and check if fields are selected, and disable if they are (on ajaxDone)
         $(document).on('ajaxDone', function (){
+            $('.variantPlacementOptions').on('change', 'select', function() {
+                $('.variantPlacementOptions select option').prop('disabled', false);
+                $('.variantPlacementOptions select').each(function() {
+                    let val = this.value;
+                    $('.variantPlacementOptions select').not(this).find('option').filter(function() {
+                        return this.value === val;
+                    }, this).prop('disabled', true);
+                });
+            }).change();
+
             $('.variantPlacementOptions select').on('change', function() {
                 $('.variantPlacementOptions select option').prop('disabled', false);
                 $('.variantPlacementOptions select').each(function() {
-                    var val = this.value;
+                    let val = this.value;
                     $('.variantPlacementOptions select').not(this).find('option').filter(function() {
                         return this.value === val;
-                    }).prop('disabled', true);
+                    }, this).prop('disabled', true);
                 });
             }).change();
         })
