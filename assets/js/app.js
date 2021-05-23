5e166bed2347e40e84fdc06139fe9a9363285fe7
Devin Green <design@devingreen.me>
2021-05-22 20:03:43 -0500

    simplify javascript.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,8 @@
 $(document).ready(function () {
     $(document).on('shown.oc.popup', function() {
         // Get form and check if fields are selected, and disable if they are
-        $('.variantPlacementOptions').on('change', 'select', function() {
-            console.log('did it work?')
-            $('.variantPlacementOptions select option').prop('disabled', false);
-            $('.variantPlacementOptions select').each(function() {
-                let val = this.value;
-                $('.variantPlacementOptions select').not(this).find('option').filter(function() {
-                    return this.value === val;
-                }, this).prop('disabled', true);
-            });
-        }).change();
 
         $('.variantPlacementOptions select').on('change', function() {
-            // console.log('did it work?')
             $('.variantPlacementOptions select option').prop('disabled', false);
             $('.variantPlacementOptions select').each(function() {
                 let val = this.value;
@@ -26,15 +15,6 @@ $(document).ready(function () {
 
         // Get form and check if fields are selected, and disable if they are (on ajaxDone)
         $(document).on('ajaxDone', function (){
-            $('.variantPlacementOptions').on('change', 'select', function() {
-                $('.variantPlacementOptions select option').prop('disabled', false);
-                $('.variantPlacementOptions select').each(function() {
-                    let val = this.value;
-                    $('.variantPlacementOptions select').not(this).find('option').filter(function() {
-                        return this.value === val;
-                    }, this).prop('disabled', true);
-                });
-            }).change();
 
             $('.variantPlacementOptions select').on('change', function() {
                 $('.variantPlacementOptions select option').prop('disabled', false);
