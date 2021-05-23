4079c0b91b6ba6b0f40fad50174acecb0c4309bc
Devin Green <design@devingreen.me>
2021-05-22 22:02:28 -0500

    code organization and optimization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,23 @@
-// automatically disable fields that are already accepted in the
-$(document).ready(function () {
-    $(document).on('shown.oc.popup', function() {
-        // Get form and check if fields are selected, and disable if they are
-
-        $('.variantPlacementOptions select').on('change', function() {
-            $('.variantPlacementOptions select option').prop('disabled', false);
-            $('.variantPlacementOptions select').each(function() {
-                let val = this.value;
-                $('.variantPlacementOptions select').not(this).find('option').filter(function() {
-                    return this.value === val;
-                }, this).prop('disabled', true);
-            });
-        }).change();
-
-        // Get form and check if fields are selected, and disable if they are (on ajaxDone)
-        $(document).on('ajaxDone', function (){
-
-            $('.variantPlacementOptions select').on('change', function() {
-                $('.variantPlacementOptions select option').prop('disabled', false);
-                $('.variantPlacementOptions select').each(function() {
-                    let val = this.value;
-                    $('.variantPlacementOptions select').not(this).find('option').filter(function() {
-                        return this.value === val;
-                    }, this).prop('disabled', true);
-                });
-            }).change();
-        })
+function setSelects() {
+    $('.variantPlacementOptions select').each(function() {
+        let val = this.value;
+        $('.variantPlacementOptions select').not(this).find('option').filter(function() {
+            return this.value === val;
+        }, this).prop('disabled', true);
     });
-})
+}
+
+function updateSelects() {
+    $('.variantPlacementOptions').on('change', 'select', function() {
+        $('.variantPlacementOptions select option').prop('disabled', false)
+        setSelects();
+    }).change();
+}
+$(document).on('shown.oc.popup', function() {
+    setSelects();
+    updateSelects();
+    $(document).on('ajaxDone', function (){
+        setSelects();
+        updateSelects()
+    }).change();
+});
